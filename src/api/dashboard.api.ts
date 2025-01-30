70e02571ac86ce27b8a2ea50d24ae1de8cfe550e
Official-Divyansh <officialdivyansh44066>
2025-01-30 13:57:30 +0530

    update product API

--- src/api/dashboard.api.ts
+++ src/api/dashboard.api.ts
@@ -1,47 +1,60 @@
 import { PrismaClient } from "@prisma/client";
 import { Response, Request } from "express";
 import Joi from "joi";
-import {checkSchema} from "../utils/schemaValidation";
+import { checkSchema } from "../utils/schemaValidation";
+import { variant, variantAttribute } from "../utils/enum";
 
 const prisma = new PrismaClient();
 
+
 export class product {
   public static async createProduct(req: Request, res: Response) {
-    const {sellerId,asABrand,categoryId,isActive} = req.body;
+    const { sellerId, asABrand, categoryId, isActive } = req.body;
+    const variant: variant[] = req.body.variant;
+    const variantAttribute: variantAttribute[] = req.body.variantAttribute;
     try {
-     const productResult = await prisma.product.create({data:{
-            isActive: isActive ? isActive : false,
-            sellerId:sellerId,
-            asABrand:asABrand,
-            categoryId:categoryId,
-            variantAttribute:{
-                a:""
-            }
-      }})
+      const productResult = await prisma.product.create({
+        data: {
+          isActive: isActive ? isActive : false,
+          sellerId: sellerId,
+          asABrand: asABrand,
+          categoryId: categoryId,
+          variantAttribute: variantAttribute,
+        },
+      });
+
+      await prisma.variantTypes.createMany({
+        data: variant,
+        skipDuplicates: true,
+      });
       res.status(200).json({
         message: "Success",
-        data:productResult
+        data: productResult,
       });
-      return
+      return;
     } catch (err) {
-        console.log(err)
+      console.log(err);
       res.status(404).send("Something went wrong");
-         return
+      return;
     }
   }
 
-  public static async fetchProducts(req:Request,res:Response){
+  public static async fetchProducts(req: Request, res: Response) {
     try {
-         const productResult = await prisma.product.findMany()
-         res.status(200).json({
-           message: "Success",
-           data:productResult
-         });
-         return
-       } catch (err) {
-           console.log(err)
-         res.status(404).send("Something went wrong");
-            return
-       }
+      const productResult = await prisma.variantTypes.findMany({
+        where:{
+          variantName: "default"
+        }
+      });
+      res.status(200).json({
+        message: "Success",
+        data: productResult,
+      });
+      return;
+    } catch (err) {
+      console.log(err);
+      res.status(404).send("Something went wrong");
+      return;
+    }
   }
 }
---
+++ src/utils/enum.ts
@@ -0,0 +1,24 @@
+export type variantAttribute = {
+  name: string;
+  values: string[];
+};
+
+export type variant = {
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  category: string;
+  subCategory: string;
+  weight: number;
+  length: number;
+  height: number;
+  width: number;
+  images: any;
+  video: string;
+  specification: any;
+  variantName: string;
+  variantId: string;
+  productId: string;
+  wasPrice: number;
+};
